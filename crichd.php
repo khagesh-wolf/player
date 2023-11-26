8de389736433c9727e45975312f3bc12cb33d54f
WOLF <126966514+khagesh-wolf@users.noreply.github.com>
2023-11-26 20:29:35 +0545

    Add files via upload

---
+++ crichd.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CricHD Player</title>
+    <style>
+        #player-container {
+            position: relative;
+            overflow: hidden;
+            padding-top: 56.25%; /* 16:9 aspect ratio (change based on your player's aspect ratio) */
+        }
+        #player-iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            border: 0;
+        }
+    </style>
+    <style>
+    #player-container {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      background: #000;
+      color: #fff;
+      overflow: hidden;
+    }
+    #iframe-iframe, #jwPlayer {
+      height: 100vh !important;
+      padding: 0;
+      margin: 0;
+    }
+    body {
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    #container {
+      position: absolute;
+      text-align: center;
+    }
+    video {
+      outline: 0;
+    }
+    #text {
+      position: fixed;
+      border: 0;
+      left: 0;
+      width: 100%;
+      color: #fff;
+      bottom: 50px;
+      text-align: center;
+      font-family: tahoma;
+      font-size: 20px;
+      z-index: 9999;
+    }
+  </style>
+  
+</head>
+<body>
+
+<div id="player-container">
+    <iframe id="player-iframe" allowfullscreen></iframe>
+      <div id="text">www.tutorialto.com</div>
+
+</div>
+<?php
+    // Get the Custom_ID from the URL parameter
+    $customID = $_GET['channel'];
+
+    // Replace (Custom_ID) in the original URL with the custom ID
+    $originalURL = "https://bigcast.xyz/embed.php?v=" . $customID;
+?>
+<script>
+    // Set the iframe source to the modified URL
+    document.getElementById('player-iframe').src = "<?php echo $originalURL; ?>";
+</script>
+
+</body>
+</html>
