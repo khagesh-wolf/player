c1deb3718c99e5eb386ba8cc909cc1e6177b2706
WOLF <126966514+khagesh-wolf@users.noreply.github.com>
2023-11-29 22:59:43 +0545

    Update crichd.php

--- crichd.php
+++ crichd.php
@@ -1,15 +1,24 @@
 <?php
+
 // Get the Custom_ID from the URL parameter
-$urlParams = $_GET['channel'];
-$customID = $urlParams;
+$customID = $_GET['src'];
 
 // Replace (Custom_ID) in the original URL with the custom ID
 $originalURL = "https://bigcast.xyz/embed.php?v=$customID";
 
 // Set the iframe source to the modified URL
 $iframeSource = $originalURL;
-?>
 
+// Generate the HTML markup for the player container
+$playerContainerHTML = <<<HTML
+<div id="player-container">
+  <iframe id="player-iframe" src="$iframeSource" allowfullscreen></iframe>
+  <div id="text">www.tutorialto.com</div>
+</div>
+HTML;
+
+// Generate the HTML markup for the entire page
+$pageHTML = <<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,10 +83,11 @@ $iframeSource = $originalURL;
 </head>
 <body>
 
-<div id="player-container">
-  <iframe id="player-iframe" src="<?= $iframeSource ?>" allowfullscreen></iframe>
-  <div id="text">www.tutorialto.com</div>
-</div>
+$playerContainerHTML
 
 </body>
 </html>
+HTML;
+
+// Echo the entire page HTML
+echo $pageHTML;
